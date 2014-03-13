4aa45523a3fd60463edb6d84c50f5c333dc4d442
chirp <chirp@camilo-XPS-L701X.(none)>
2014-03-12 18:04:40 -0600

    Fixed Part2, recorded data

--- Part2/Part2PC.java
+++ Part2/Part2PC.java
@@ -1,4 +1,3 @@
-
 /*
  * September 21, 2009
  * Author Tawat Atigarbodee
@@ -26,10 +25,6 @@
 
 import java.io.*;
 import java.util.ArrayList;
-import java.awt.event.KeyListener;
-import java.awt.event.KeyEvent;
-
-import javax.swing.JFrame;
 
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.nxt.*;
@@ -40,62 +35,68 @@ class PointValue {
 	public double x;
 	public double y;
 	public int light;
-	
-	PointValue(double xinit, double yinit, int lightinit) 
-	{
+
+	PointValue(double xinit, double yinit, int lightinit) {
 		this.x = xinit;
 		this.y = yinit;
 		this.light = lightinit;
 	}
+
+	public String getHeader() {
+		return "@RELATION\tHeightMapping\n\n" + "@ATTRIBUTE\tx\tNUMERIC\n"
+				+ "@ATTRIBUTE\ty\tNUMERIC\n" + "@ATTRIBUTE\tlight\tNUMERIC\n\n";
+	}
+
+	public String toString() {
+		return x + "," + y + "," + light + "\n";
+	}
 }
 
 public class Part2PC {
-	private enum Mode {
-		Record, Pause, Test
-	}
-	public Mode mode = Mode.Record;
+	public UIHandler.Mode mode = UIHandler.Mode.Record;
 	public static TrackerReader tracker;
 	public static DataOutputStream outData;
 	public static NXTConnector link;
 	public static DifferentialPilot pilot;
 	public static LightSensor light;
+	public static UIHandler ui;
 	public ArrayList<PointValue> pointVals;
 
 	public Part2PC() {
 		pointVals = new ArrayList<PointValue>();
-		pilot = new DifferentialPilot(5.5f, 5.5f, 
-							Motor.A, Motor.C);
+		ui = new UIHandler(this);
+		pilot = new DifferentialPilot(5.5f, 5.5f, Motor.A, Motor.C);
 		tracker = new TrackerReader();
 		light = new LightSensor(SensorPort.S2, true);
 		pilot.setTravelSpeed(1);
 		tracker.start();
-		
-		while (true) 
-		{
-			System.out.println(this.mode.toString());
-			switch (mode)
-			{
-				case Record:
-					recordDataPoint();
-					break;
-				case Pause:
-					
-					break;
-				case Test:
-					
-				break; 
-				default:
-					
+
+		System.out
+				.println("Please run 'python tracker.py' and select the colour");
+
+		// wait until the tracker has made a connection to the camera
+		while (!tracker.hasConnection)
+			Delay.msDelay(500);
+
+		while (true) {
+			mode = ui.getMode();
+			switch (mode) {
+			case Record:
+				recordDataPoint();
+				break;
+			case Pause:
+				showTestData();
+				break;
+			case Test:
+
+				break;
+			default:
 			}
 		}
 	}
 	
-	public void recordDataPoint() {
-		// move the robot into a new position for recording a data point
-		Delay.msDelay(500);
-		pilot.travel(0.5);
-		
-		/* possible tracker values: x, y, a, theta, targetx, targety */
+	// print the data that is being received from tracker to syso
+	public void showTestData() {
 		double x = tracker.x;
 		double y = tracker.y;
 		double theta = tracker.theta;
@@ -103,18 +104,64 @@ public class Part2PC {
 		double targety = tracker.targety;
 		int l = light.getLightValue();
 		
+		System.out.println("x: " + x + " y: " + y + " theta: " + theta);
+		System.out.println("trackerx: " + targetx + " trackery: " + targety);
+		System.out.println("light: " + l);
+	}
+	
+	public void recordDataPoint() {
+		/* possible tracker values: x, y, a, theta, targetx, targety */
+		double x = tracker.x;
+		double y = tracker.y;
+		double theta = tracker.theta;
+		double targetx = tracker.targetx;
+		double targety = tracker.targety;
+		int l = light.getLightValue();
+
+		// dont record if data == 0.0 because no tracker.py colour has been
+		// selected
+		if (x == 0.0 && y == 0.0 && theta == 0.0 && targetx == 0.0
+				&& targety == 0.0)
+			return;
+
 		// record the data point
-		System.out.println("x = " + tracker.x + 
-						  " y = " + tracker.y + " light = " + l);
+		System.out.println("x = " + tracker.x + " y = " + tracker.y
+				+ " light = " + l);
 		pointVals.add(new PointValue(x, y, l));
+		System.out.println("Num data pts: " + pointVals.size());
+		// move the robot into a new position for next data recording call
+		Delay.msDelay(500);
+		pilot.travel(0.5);
 	}
 
 	public void saveData() {
-		
+		writeArffFile(pointVals);
 	}
-    
-	public static void main(String[] args) 
-	{
+
+	private void writeArffFile(ArrayList<PointValue> results) {
+		String filename = "p2output-" + System.currentTimeMillis() + ".arff";
+
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(filename));
+
+			// Header!
+			out.write(results.get(0).getHeader());
+
+			// Data!
+			out.write("@DATA\n");
+			for (int i = 0; i < results.size(); ++i) {
+				out.write(results.get(i).toString());
+			}
+
+			out.close();
+		} catch (IOException e) {
+
+		}
+
+		System.out.println("Saved as " + filename + "\n");
+	}
+
+	public static void main(String[] args) {
 		Part2PC pc = new Part2PC();
 	}
 
---
+++ Part2/UIHandler.java
@@ -0,0 +1,90 @@
+import java.awt.BorderLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+//Here's a class for a simple GUI that uses a JFrame
+//to hold to JTextAreas - one will listen for the key events
+//and the other will sit inside a JScrollPane providing feedback
+//about the KeyListener events being triggered
+
+public class UIHandler {
+
+	public static enum Mode {
+		Record, Pause, Test
+	}
+
+	Part2PC parent;
+	public Mode mode;
+	JTextArea cmdText;
+	JTextArea modeDisplay;
+
+	public UIHandler(Part2PC p2) {
+		this.parent = p2;
+		JFrame guiFrame = new JFrame();
+		this.mode = Mode.Pause;
+
+		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		guiFrame.setTitle("Creating a Table Example");
+		guiFrame.setSize(400, 118);
+		guiFrame.setLocationRelativeTo(null);
+
+		// add a text area to show what the current mode is
+		modeDisplay = new JTextArea();
+		updateModeDisplay();
+
+		// set up the text area
+		int rows = 4;
+		int cols = 50;
+		cmdText = new JTextArea(rows, cols);
+		cmdText.setEditable(false);
+		cmdText.append("Press:\t't' to enter test mode\n");
+		cmdText.append("\t'r' to enter record mode\n");
+		cmdText.append("\t'p' to pause\n");
+		cmdText.append("\t's' to save recorded data from record mode\n");
+
+		// add a keylistener to the text box
+		cmdText.addKeyListener(new KeyListener() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				switch (e.getKeyChar()) {
+				case 't':
+					mode = Mode.Test;
+					break;
+				case 'r':
+					mode = Mode.Record;
+					break;
+				case 'p':
+					mode = Mode.Pause;
+					break;
+				case 's':
+					parent.saveData();
+					break;
+				default:
+				}
+				updateModeDisplay();
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+			}
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+		});
+
+		guiFrame.add(cmdText, BorderLayout.NORTH);
+		guiFrame.add(modeDisplay, BorderLayout.SOUTH);
+		guiFrame.setVisible(true);
+	}
+
+	private void updateModeDisplay() {
+		modeDisplay.setText("Current Mode: [" + mode.toString() + "]");
+	}
+
+	public Mode getMode() {
+		return mode;
+	}
+}
